9f6a0985a7d2b6e85a486a534b7e99980b003d42
gustavoalves23 <gustavo.alves388@gmail.com>
2022-02-11 11:54:51 -0300

    go

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -1,23 +1,28 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'database_development',
-    host: '127.0.0.1',
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    port:process.env.DB_PORT,
     dialect: 'postgres',
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    port:process.env.DB_PORT,
     dialect: 'postgres',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    port:process.env.DB_PORT,
     dialect: 'postgres',
   },
 };

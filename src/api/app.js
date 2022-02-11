9c35baa6311fb5be4d6174b3c83cba232b44b78a
gustavoalves23 <gustavo.alves388@gmail.com>
2022-02-11 12:21:07 -0300

    go

--- src/api/app.js
+++ src/api/app.js
@@ -4,7 +4,9 @@ const UserController = require('../controllers/user');
 const errorMiddleware = require('../middlewares/error');
 const cors = require('cors');
 
-const app = express(cors());
+const app = express();
+
+app.use(cors());
 
 app.get('/ping', (req, res) => res.status(200).send('pong'));
 

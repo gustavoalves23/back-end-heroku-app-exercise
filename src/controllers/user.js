2abc48693843eaa2f518a4969190ca9f96cfd3bf
gustavoalves23 <gustavo.alves388@gmail.com>
2022-02-11 12:47:40 -0300

    action

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,5 +2,5 @@ const { User } = require('../models');
 
 module.exports = async (_req, res) => {
     const users = await User.findAll();
-    res.status(200).json({users});
+    res.status(200).json(users);
 };
